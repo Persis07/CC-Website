cc29ae5fe25430ea17e6483ab128f4e7146a50b2
Persis <125876497+Persis07@users.noreply.github.com>
2024-01-02 17:24:38 +0100

    Update test.js

--- test.js
+++ test.js
@@ -27,3 +27,27 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+ <header>
+        <img src="./assets/DS_Hauptbild_2200x1238.jpg" alt="ds image">
+        <h1>Food</h1>
+        <p>Project Description Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </header>
+
+    <div class="cards-container">
+        <div class="card left">
+            <img src="./Asset_services animation Projekt_AME/DS_Casesbild_1_822x596.jpg" alt="Image 1">
+            </div>
+        <div class="card right">
+            <h2>Food Packaging</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi aperiam blanditiis
+                distinctio incidunt minima provident eius, maiores unde! Exercitationem
+                recusandae aperiam soluta, unde sequi doloremque nihil praesentium perferendis ratione aut.
+            Oorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia laboriosam vero in exercitationem
+            molestiae, ipsa, quo nesciunt dicta nisi dolorum neque incidunt. Nam mollitia nisi animi itaque doloribus reprehenderit sed.</p>
+        </div>
+        
+
+</div>
+
